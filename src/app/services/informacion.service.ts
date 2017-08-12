31b5dd0aef369c70d27a9ae21cf06b769fbf0e40
Olguiver Maya <olguiver@gmail.com>
2017-08-12 14:35:56 -0500

    Servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,15 +5,34 @@ import { Http } from "@angular/http";
 export class InformacionService {
 info:any = {};
 cargada:boolean = false;
+cargada_sobre_nostros:boolean =false;
+equipo:any[] = [];
 
   constructor( public http:Http ) {
-    this.http.get("assets/data/info.pagina.json")
-              .subscribe( data => {
-                console.log(data.json());
-                this.cargada = true;
-                this.info = data.json();
-              })
+    this.carga_info();
+    this.carga_sobre_nosotros();
 
    }
+
+public carga_info(){
+  this.http.get("assets/data/info.pagina.json")
+            .subscribe( data => {
+              //console.log(data.json());
+              this.cargada = true;
+              this.info = data.json();
+            });
+}
+
+
+public carga_sobre_nosotros(){
+  this.http.get("https://paginaweb-d5cb2.firebaseio.com/equipo.json")
+            .subscribe( data => {
+              console.log(data.json());
+              this.cargada_sobre_nostros = true;
+              this.equipo = data.json();
+            });
+}
+
+
 
 }
