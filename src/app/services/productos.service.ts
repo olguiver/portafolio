073e27536f4775c6b5c3249fa4c887879ad87a79
Olguiver Maya <olguiver@gmail.com>
2017-08-16 21:41:49 -0500

    Agragamos firebase a el portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -16,10 +16,11 @@ export class ProductosService {
     //if ( this.productos.length == 0){
       this.http.get('https://paginaweb-d5cb2.firebaseio.com/productos_idx.json')
         .subscribe( res => {
-          console.log( res.json());
+        //  console.log( res.json());
           this.cargando = false;
+          this.productos = res.json();
 
-        })
+        });
     }
   //}
 
